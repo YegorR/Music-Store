49c991b77eac9c5417913c5f7401204bf0ca70db
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-02-27 16:04:26 +0700

    Add debug output

---
+++ src/main/java/ru/yegorr/musicstore/log/RequestLogConfig.java
@@ -0,0 +1,22 @@
+package ru.yegorr.musicstore.log;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RequestLogConfig implements WebMvcConfigurer {
+
+    private RequestLogInterceptor requestLogInterceptor;
+
+    @Autowired
+    public RequestLogConfig(RequestLogInterceptor requestLogInterceptor) {
+        this.requestLogInterceptor = requestLogInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLogInterceptor);
+    }
+}
---
+++ src/main/java/ru/yegorr/musicstore/log/RequestLogInterceptor.java
@@ -0,0 +1,20 @@
+package ru.yegorr.musicstore.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class RequestLogInterceptor extends HandlerInterceptorAdapter {
+    private Logger log = LoggerFactory.getLogger(RequestLogInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        log.debug(request.getRequestURI());
+        return super.preHandle(request, response, handler);
+    }
+}
