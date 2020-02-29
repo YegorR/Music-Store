896a995c9da0a514871ea65dcdeb3ecd0407258c
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-02-29 13:55:23 +0700

    Add UserEntity

---
+++ src/main/java/ru/yegorr/musicstore/entity/UserEntity.java
@@ -0,0 +1,78 @@
+package ru.yegorr.musicstore.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_account")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "nickname", unique = true, nullable = false)
+    private String nickname;
+
+    @Column(name = "salt", nullable = false, length = 64)
+    private String salt;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "is_admin", nullable = false)
+    private Boolean isAdmin;
+
+    public UserEntity() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+}
