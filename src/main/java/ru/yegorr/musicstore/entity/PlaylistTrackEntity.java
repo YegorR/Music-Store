8443d72fe22a8b57150c0030b74b2b64f94709d2
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-03-29 20:43:57 +0700

    Fix PlaylistTrackEntity according to database

--- src/main/java/ru/yegorr/musicstore/entity/PlaylistTrackEntity.java
+++ src/main/java/ru/yegorr/musicstore/entity/PlaylistTrackEntity.java
@@ -10,6 +10,20 @@ public class PlaylistTrackEntity {
     @EmbeddedId
     private PlaylistTrackId playlistTrackId;
 
+    @MapsId("playlistId")
+    @ManyToOne
+    @JoinColumn(name = "playlist_id")
+    private PlaylistEntity playlist;
+
+    @MapsId("trackId")
+    @ManyToOne
+    @JoinColumn(name = "track_id")
+    private TrackEntity track;
+
+    @MapsId("order")
+    @Column(name = "order")
+    private Integer order;
+
     public PlaylistTrackEntity() {
     }
 
@@ -21,37 +35,56 @@ public class PlaylistTrackEntity {
         this.playlistTrackId = playlistTrackId;
     }
 
+    public PlaylistEntity getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(PlaylistEntity playlist) {
+        this.playlist = playlist;
+    }
+
+    public TrackEntity getTrack() {
+        return track;
+    }
+
+    public void setTrack(TrackEntity track) {
+        this.track = track;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
 
     @Embeddable
     public static class PlaylistTrackId implements Serializable {
-        @ManyToOne
-        @JoinColumn(name = "playlist_id")
-        private PlaylistEntity playlist;
 
-        @ManyToOne
-        @JoinColumn(name = "track_id")
-        private TrackEntity track;
+        private Long playlistId;
 
-        @Column(name = "order")
+        private Long trackId;
+
         private Integer order;
 
         public PlaylistTrackId() {
         }
 
-        public PlaylistEntity getPlaylist() {
-            return playlist;
+        public Long getPlaylistId() {
+            return playlistId;
         }
 
-        public void setPlaylist(PlaylistEntity playlist) {
-            this.playlist = playlist;
+        public void setPlaylistId(Long playlist) {
+            this.playlistId = playlist;
         }
 
-        public TrackEntity getTrack() {
-            return track;
+        public Long getTrackId() {
+            return trackId;
         }
 
-        public void setTrack(TrackEntity track) {
-            this.track = track;
+        public void setTrackId(Long trackId) {
+            this.trackId = trackId;
         }
 
         public Integer getOrder() {
@@ -67,14 +100,14 @@ public class PlaylistTrackEntity {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             PlaylistTrackId that = (PlaylistTrackId) o;
-            return Objects.equals(playlist, that.playlist) &&
-                    Objects.equals(track, that.track) &&
+            return Objects.equals(playlistId, that.playlistId) &&
+                    Objects.equals(trackId, that.trackId) &&
                     Objects.equals(order, that.order);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(playlist, track, order);
+            return Objects.hash(playlistId, trackId, order);
         }
     }
 }
