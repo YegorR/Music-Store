b235879d5416ef3fc134a486b10525f8c16623d9
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-10 23:54:07 +0700

    Create ResponseDto

---
+++ src/main/java/ru/yegorr/musicstore/dto/ResponseDto.java
@@ -0,0 +1,33 @@
+package ru.yegorr.musicstore.dto;
+
+public class ResponseDto {
+    private int code;
+
+    private Object body;
+
+    private String error;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
