5b200ab30ce11e3cbfbec948c059a636d5f8cd79
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-10 23:02:38 +0700

    Create UserRegistrationDto

--- src/main/java/ru/yegorr/musicstore/controller/AuthController.java
+++ src/main/java/ru/yegorr/musicstore/controller/AuthController.java
@@ -3,12 +3,13 @@ package ru.yegorr.musicstore.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yegorr.musicstore.dto.UserRegistrationDto;
 
 @RestController
 public class AuthController {
 
     @PostMapping("/register")
-    public ResponseEntity<?> register() {
+    public ResponseEntity<?> register(UserRegistrationDto userRegistration) {
         return ResponseEntity.status(200).body("Not working");
     }
 
---
+++ src/main/java/ru/yegorr/musicstore/dto/UserRegistrationDto.java
@@ -0,0 +1,33 @@
+package ru.yegorr.musicstore.dto;
+
+public class UserRegistrationDto {
+    private String email;
+
+    private String nickname;
+
+    private byte[] password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public byte[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(byte[] password) {
+        this.password = password;
+    }
+}
