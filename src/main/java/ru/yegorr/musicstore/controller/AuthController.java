2abc8fb2b14ca45273f07876ab6538f042bee2c5
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-11 00:00:04 +0700

    Add AuthService to AuthController

--- src/main/java/ru/yegorr/musicstore/controller/AuthController.java
+++ src/main/java/ru/yegorr/musicstore/controller/AuthController.java
@@ -1,21 +1,33 @@
 package ru.yegorr.musicstore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yegorr.musicstore.dto.UserLoginDto;
 import ru.yegorr.musicstore.dto.UserRegistrationDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
+import ru.yegorr.musicstore.service.AuthService;
 
 @RestController
 public class AuthController {
 
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/register")
-    public ResponseEntity<?> register(UserRegistrationDto userRegistration) {
+    public ResponseEntity<?> register(UserRegistrationDto userRegistration) throws ApplicationException {
+        authService.register(userRegistration);
         return ResponseEntity.status(200).body("Not working");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(UserLoginDto userLogin) {
-        return ResponseEntity.status(200).body("Not working");
+    public ResponseEntity<?> login(UserLoginDto userLogin) throws ApplicationException {
+        String token = authService.login(userLogin);
+        return ResponseEntity.status(200).body(token);
     }
 }
