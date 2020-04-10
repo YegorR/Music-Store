6cf18fb0b996c31519366f8961e10b3292f9760f
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-10 23:16:14 +0700

    Add exception throwing in AuthService

--- src/main/java/ru/yegorr/musicstore/service/AuthService.java
+++ src/main/java/ru/yegorr/musicstore/service/AuthService.java
@@ -2,9 +2,10 @@ package ru.yegorr.musicstore.service;
 
 import ru.yegorr.musicstore.dto.UserLoginDto;
 import ru.yegorr.musicstore.dto.UserRegistrationDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
 
 public interface AuthService {
-    void register(UserRegistrationDto userRegistration);
+    void register(UserRegistrationDto userRegistration) throws ApplicationException;
 
-    String login(UserLoginDto userLogin);
+    String login(UserLoginDto userLogin) throws ApplicationException;
 }
