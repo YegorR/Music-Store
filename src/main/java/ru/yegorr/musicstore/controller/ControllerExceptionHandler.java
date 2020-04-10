ddac3bfdd4ca1207b1c5ab8b2c49c2bc333ff727
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-10 23:56:34 +0700

    Create ControllerExceptionHandler
    ControllerExceptionHandler handles the exceptions throwing by controllers and sends the responses with error message.

---
+++ src/main/java/ru/yegorr/musicstore/controller/ControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package ru.yegorr.musicstore.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.yegorr.musicstore.dto.ResponseDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ApplicationException.class)
+    protected ResponseEntity<?> handleApplicationException(ApplicationException ex) {
+        ResponseDto response = new ResponseDto();
+        response.setCode(500);
+        response.setError("Something wrong on the server");
+        return ResponseEntity.status(500).body(response);
+    }
+}
