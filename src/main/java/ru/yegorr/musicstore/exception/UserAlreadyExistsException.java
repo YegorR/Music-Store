29cd2388514ebd23d2bc493d0be2e152e0eb5e77
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-11 21:23:06 +0700

    Revert "Create UserAlreadyExistsException"
    This reverts commit dc18b14c

--- src/main/java/ru/yegorr/musicstore/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.yegorr.musicstore.exception;
-
-public class UserAlreadyExistsException extends ApplicationException {
-    public UserAlreadyExistsException(String msg) {
-        super(msg);
-    }
-}
