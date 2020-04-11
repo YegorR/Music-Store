9fe64fb0c33f0c216103e728256876e3dde410b2
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-12 00:28:09 +0700

    Change HistoryEntity
    Change id from embedded to normal

--- src/main/java/ru/yegorr/musicstore/entity/HistoryEntity.java
+++ src/main/java/ru/yegorr/musicstore/entity/HistoryEntity.java
@@ -8,15 +8,16 @@ import java.util.Objects;
 @Entity
 @Table(name = "history")
 public class HistoryEntity {
-    @EmbeddedId
-    private HistoryId historyId;
 
-    @MapsId("userId")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "history_id")
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
-    @MapsId("trackId")
     @ManyToOne
     @JoinColumn(name = "track_id")
     private TrackEntity track;
@@ -27,28 +28,28 @@ public class HistoryEntity {
     public HistoryEntity() {
     }
 
-    public HistoryId getHistoryId() {
-        return historyId;
+    public Long getId() {
+        return id;
     }
 
-    public void setHistoryId(HistoryId historyId) {
-        this.historyId = historyId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public UserEntity getUser() {
         return user;
     }
 
-    public void setUser(UserEntity userEntity) {
-        this.user = userEntity;
+    public void setUser(UserEntity user) {
+        this.user = user;
     }
 
     public TrackEntity getTrack() {
         return track;
     }
 
-    public void setTrack(TrackEntity trackEntity) {
-        this.track = trackEntity;
+    public void setTrack(TrackEntity track) {
+        this.track = track;
     }
 
     public Timestamp getPlayTime() {
@@ -57,45 +58,5 @@ public class HistoryEntity {
 
     public void setPlayTime(Timestamp playTime) {
         this.playTime = playTime;
-    }
-
-    @Embeddable
-    public static class HistoryId implements Serializable {
-        private Integer userId;
-
-        private Integer trackId;
-
-        public HistoryId() {
-        }
-
-        public Integer getUserId() {
-            return userId;
-        }
-
-        public void setUserId(Integer userId) {
-            this.userId = userId;
-        }
-
-        public Integer getTrackId() {
-            return trackId;
-        }
-
-        public void setTrackId(Integer trackId) {
-            this.trackId = trackId;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            HistoryId historyId = (HistoryId) o;
-            return Objects.equals(userId, historyId.userId) &&
-                    Objects.equals(trackId, historyId.trackId);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(userId, trackId);
-        }
     }
 }
