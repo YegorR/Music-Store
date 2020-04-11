f85dae5daa899fe144af62f6ee6d99c778abb10d
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-11 21:35:53 +0700

    Add UserRepository attribute and add UserIsAlreadyExistsException throwing in register()

--- src/main/java/ru/yegorr/musicstore/service/AuthServiceImpl.java
+++ src/main/java/ru/yegorr/musicstore/service/AuthServiceImpl.java
@@ -1,14 +1,34 @@
 package ru.yegorr.musicstore.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yegorr.musicstore.dto.UserLoginDto;
 import ru.yegorr.musicstore.dto.UserRegistrationDto;
 import ru.yegorr.musicstore.exception.ApplicationException;
+import ru.yegorr.musicstore.exception.UserIsAlreadyExistsException;
+import ru.yegorr.musicstore.repository.UserRepository;
 
 @Service
 public class AuthServiceImpl implements AuthService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void register(UserRegistrationDto userRegistration) throws ApplicationException {
+        if (userRepository.countAllByEmail(userRegistration.getEmail()) != 0) {
+            throw new UserIsAlreadyExistsException("User with this email already exists");
+        }
+        if (userRepository.countAllByNickname(userRegistration.getNickname()) != 0) {
+            throw new UserIsAlreadyExistsException("User with this nickname already exists");
+        }
+        // Генерируем соль и хеш
+
+        // Сохраняем всё в бд
     }
 
     @Override
