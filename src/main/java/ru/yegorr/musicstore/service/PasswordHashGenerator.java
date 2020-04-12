36d3a187eaf6d5b5aa9991443562bb80e77e8dd4
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-12 19:30:39 +0700

    Change PasswordHashGenerator

--- src/main/java/ru/yegorr/musicstore/service/PasswordHashGenerator.java
+++ src/main/java/ru/yegorr/musicstore/service/PasswordHashGenerator.java
@@ -1,22 +1,20 @@
 package ru.yegorr.musicstore.service;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import at.favre.lib.crypto.bcrypt.LongPasswordStrategies;
+
 
 import java.security.SecureRandom;
 
 public class PasswordHashGenerator {
     private final SecureRandom randomizer = new SecureRandom();
     public byte[] generateRandomSalt() {
-        byte[] salt = new byte[64];
+        byte[] salt = new byte[16];
         randomizer.nextBytes(salt);
         return salt;
     }
 
     public byte[] generateHash(byte[] password, byte[] salt) {
-        byte[] passwordWithSalt = new byte[password.length + salt.length];
-        System.arraycopy(password, 0, passwordWithSalt, 0, password.length);
-        System.arraycopy(salt, 0, passwordWithSalt, password.length, salt.length);
-
-        return BCrypt.withDefaults().hash(12, passwordWithSalt);
+        return BCrypt.with(LongPasswordStrategies.hashSha512(BCrypt.Version.VERSION_2A)).hash(12, salt, password);
     }
 }
