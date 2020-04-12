07cc0404b9b14fa7797dc924c84eb8acd1afb7ca
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-12 22:12:06 +0700

    Add check method

--- src/main/java/ru/yegorr/musicstore/service/TokenHandler.java
+++ src/main/java/ru/yegorr/musicstore/service/TokenHandler.java
@@ -1,14 +1,28 @@
 package ru.yegorr.musicstore.service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class TokenHandler {
     private static final String KEY = "MY_SECRET_KEY";
 
     private static final Algorithm algorithm = Algorithm.HMAC256(KEY);
 
+    private static final JWTVerifier verifier = JWT.require(algorithm).build();
+
     public String getToken(Long userId) {
         return JWT.create().withClaim("id", userId).sign(algorithm);
+    }
+
+    public Long checkToken(String token) {
+        try {
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getClaim("id").asLong();
+        } catch (JWTVerificationException ex) {
+            return null;
+        }
     }
 }
