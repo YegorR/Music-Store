64e99fcb30a0258172a1ee410d9e584beb609154
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-12 20:44:08 +0700

    Create LoginResponseDto

---
+++ src/main/java/ru/yegorr/musicstore/dto/LoginResponseDto.java
@@ -0,0 +1,43 @@
+package ru.yegorr.musicstore.dto;
+
+public class LoginResponseDto {
+    private String token;
+
+    private Long id;
+
+    private String nickname;
+
+    private boolean admin;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+}
