b3e9d1035d3d2dac044dd788beec91c76887de20
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-14 01:16:22 +0700

    Create createMusician body

--- src/main/java/ru/yegorr/musicstore/controller/MusicianController.java
+++ src/main/java/ru/yegorr/musicstore/controller/MusicianController.java
@@ -2,17 +2,44 @@ package ru.yegorr.musicstore.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.yegorr.musicstore.dto.MusicianDto;
+import ru.yegorr.musicstore.dto.MusicianResponseDto;
+import ru.yegorr.musicstore.dto.ResponseDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
+import ru.yegorr.musicstore.exception.ForbiddenException;
+import ru.yegorr.musicstore.service.AuthService;
+import ru.yegorr.musicstore.service.MusicianService;
 
 @Controller
 public class MusicianController {
 
-    @PostMapping("/musician")
-    public ResponseEntity<?> createMusician() {
-        return null;
+    private final MusicianService musicianService;
+
+    private final AuthService authService;
+
+    public MusicianController(MusicianService musicianService, AuthService authService) {
+        this.authService = authService;
+        this.musicianService = musicianService;
+    }
+
+    @PostMapping(path = "/musician",
+            consumes = "application/json",
+            produces = "application/json")
+    public ResponseEntity<?> createMusician(@RequestBody MusicianDto musicianDto,
+                                            @RequestHeader("Authorization") String token) throws ApplicationException {
+        ActualUserInformation userInformation = authService.check(token);
+        if (!userInformation.getAdmin()) {
+            throw new ForbiddenException("You have no rights to do this");
+        }
+
+        MusicianResponseDto musicianResponseDto =
+                musicianService.createMusician(musicianDto.getName(), musicianDto.getDescription());
+
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setBody(musicianResponseDto);
+        responseDto.setCode(201);
+        return ResponseEntity.status(201).body(responseDto);
     }
 
     @PutMapping("/musician/{musicianId}")
