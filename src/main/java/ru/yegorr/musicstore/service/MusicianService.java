0b0e9aff1a5046072f5c4f327d95344b78b1999d
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-14 00:14:44 +0700

    Create MusicianService

---
+++ src/main/java/ru/yegorr/musicstore/service/MusicianService.java
@@ -0,0 +1,14 @@
+package ru.yegorr.musicstore.service;
+
+import ru.yegorr.musicstore.dto.MusicianResponseDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
+
+public interface MusicianService {
+    MusicianResponseDto createMusician(String name, String description) throws ApplicationException;
+
+    MusicianResponseDto changeMusician(Long id, String name, String description) throws ApplicationException;
+
+    void deleteMusician(Long id) throws ApplicationException;
+
+    MusicianResponseDto getMusician(Long id) throws ApplicationException;
+}
