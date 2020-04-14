3d208b51f45d06b48a5888ba26fb97ec69a2fe39
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-15 01:30:29 +0700

    Create UserChecker

---
+++ src/main/java/ru/yegorr/musicstore/controller/UserChecker.java
@@ -0,0 +1,30 @@
+package ru.yegorr.musicstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.yegorr.musicstore.exception.ApplicationException;
+import ru.yegorr.musicstore.exception.ForbiddenException;
+import ru.yegorr.musicstore.service.AuthService;
+
+@Component
+public class UserChecker {
+    private final AuthService authService;
+
+    @Autowired
+    public UserChecker(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public boolean isAdmin(String token) throws ApplicationException {
+        ActualUserInformation userInformation = authService.check(token);
+        if (!userInformation.getAdmin()) {
+            throw new ForbiddenException("You have no rights to do this");
+        }
+        return userInformation.getAdmin();
+    }
+
+    public Long getUserId(String token) throws ApplicationException {
+        ActualUserInformation userInformation = authService.check(token);
+        return userInformation.getUserId();
+    }
+}
