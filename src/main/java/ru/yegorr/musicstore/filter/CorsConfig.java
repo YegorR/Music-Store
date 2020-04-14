1fd6fb84f7306a5711ff03cdfddc27349cb88e89
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-15 01:20:53 +0700

    Add CORS filter

---
+++ src/main/java/ru/yegorr/musicstore/filter/CorsConfig.java
@@ -0,0 +1,18 @@
+package ru.yegorr.musicstore.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
+        FilterRegistrationBean<CorsFilter> corsFilter = new FilterRegistrationBean<>();
+        corsFilter.setOrder(Ordered.LOWEST_PRECEDENCE);
+        corsFilter.setFilter(new CorsFilter());
+        corsFilter.addUrlPatterns("/*");
+        return corsFilter;
+    }
+}
