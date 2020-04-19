d6eea63ddc1d9723dbcb9f14e312fd8a2e9c4780
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-19 19:45:50 +0700

    Use ResponseBuilder

--- src/main/java/ru/yegorr/musicstore/controller/ControllerExceptionHandler.java
+++ src/main/java/ru/yegorr/musicstore/controller/ControllerExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import ru.yegorr.musicstore.dto.response.ResponseDto;
+import ru.yegorr.musicstore.dto.response.ResponseBuilder;
 import ru.yegorr.musicstore.exception.ClientException;
 
 
@@ -17,17 +17,18 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<?> handleApplicationException(Exception ex) {
-        ResponseDto response = new ResponseDto();
+        int code = 500;
+        String error = "Something wrong on the server";
+
         if (ex instanceof ClientException) {
             log.debug(ex.toString());
-            response.setCode(400);
-            response.setError(ex.getMessage());
+            code = 400;
+            error = ex.getMessage();
         } else {
             log.error(ex.toString());
             ex.printStackTrace();
-            response.setCode(500);
-            response.setError("Something wrong on the server");
         }
-        return ResponseEntity.status(response.getCode()).body(response);
+
+        return ResponseBuilder.getBuilder().code(code).error(error).getResponseEntity();
     }
 }
