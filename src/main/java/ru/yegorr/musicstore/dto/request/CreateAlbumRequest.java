9f5ca008f9d4e869251bc2a5434db941cbd6f346
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-19 21:17:43 +0700

    Create CreateAlbumRequest

---
+++ src/main/java/ru/yegorr/musicstore/dto/request/CreateAlbumRequest.java
@@ -0,0 +1,71 @@
+package ru.yegorr.musicstore.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+import java.util.List;
+
+public class CreateAlbumRequest {
+    private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date releaseDate;
+
+    private boolean isSingle;
+
+    private Long musicianId;
+
+    private List<Track> tracks;
+
+    public static class Track {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isSingle() {
+        return isSingle;
+    }
+
+    public void setSingle(boolean single) {
+        isSingle = single;
+    }
+
+    public Long getMusicianId() {
+        return musicianId;
+    }
+
+    public void setMusicianId(Long musicianId) {
+        this.musicianId = musicianId;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+}
