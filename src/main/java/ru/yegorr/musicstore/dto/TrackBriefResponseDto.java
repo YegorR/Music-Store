d4b6a42f61eeb5f123976237cc7e6752d6a8522d
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-19 20:50:49 +0700

    Create TrackBriefResponseDto

---
+++ src/main/java/ru/yegorr/musicstore/dto/TrackBriefResponseDto.java
@@ -0,0 +1,43 @@
+package ru.yegorr.musicstore.dto;
+
+public class TrackBriefResponseDto {
+    private Long id;
+
+    private String name;
+
+    private int playsNumber;
+
+    private boolean isFavourite;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlaysNumber() {
+        return playsNumber;
+    }
+
+    public void setPlaysNumber(int playsNumber) {
+        this.playsNumber = playsNumber;
+    }
+
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        isFavourite = favourite;
+    }
+}
