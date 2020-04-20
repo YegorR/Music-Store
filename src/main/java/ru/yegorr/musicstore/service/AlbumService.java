5b0e2ab0d7f547bb7adb43515055ab30a536b124
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-20 18:23:23 +0700

    Add changeAlbum method to AlbumService

--- src/main/java/ru/yegorr/musicstore/service/AlbumService.java
+++ src/main/java/ru/yegorr/musicstore/service/AlbumService.java
@@ -1,9 +1,12 @@
 package ru.yegorr.musicstore.service;
 
+import ru.yegorr.musicstore.dto.request.ChangeAlbumRequestDto;
 import ru.yegorr.musicstore.dto.request.CreateAlbumRequestDto;
 import ru.yegorr.musicstore.dto.response.AlbumResponseDto;
 import ru.yegorr.musicstore.exception.ApplicationException;
 
 public interface AlbumService {
     AlbumResponseDto createAlbum(CreateAlbumRequestDto createAlbumRequest, Long musicianId) throws ApplicationException;
+
+    AlbumResponseDto changeAlbum(ChangeAlbumRequestDto changeAlbumRequest, Long albumId) throws ApplicationException;
 }
