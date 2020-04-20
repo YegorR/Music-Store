fc6ddb9753cba6fb76841e89e6513a46b1cd7c7d
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-20 18:21:57 +0700

    Create ChangeAlbumRequestDto

---
+++ src/main/java/ru/yegorr/musicstore/dto/request/ChangeAlbumRequestDto.java
@@ -0,0 +1,56 @@
+package ru.yegorr.musicstore.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChangeAlbumRequestDto {
+    private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate releaseDate;
+
+    private boolean isSingle;
+
+    private List<Track> tracks = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isSingle() {
+        return isSingle;
+    }
+
+    public void setSingle(boolean single) {
+        isSingle = single;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public static class Track {
+        private Long id;
+
+        private String name;
+    }
+}
