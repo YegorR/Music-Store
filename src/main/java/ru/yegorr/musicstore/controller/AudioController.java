ea47ed6c62cea67350f90757a73236947e406b84
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-23 14:08:33 +0700

    Create AudioController

---
+++ src/main/java/ru/yegorr/musicstore/controller/AudioController.java
@@ -0,0 +1,49 @@
+package ru.yegorr.musicstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.yegorr.musicstore.dto.response.ResponseBuilder;
+import ru.yegorr.musicstore.exception.ApplicationException;
+import ru.yegorr.musicstore.exception.ForbiddenException;
+import ru.yegorr.musicstore.exception.ResourceIsNotFoundException;
+import ru.yegorr.musicstore.service.TrackService;
+
+@RestController
+public class AudioController {
+    private final TrackService trackService;
+
+    private final UserChecker userChecker;
+
+    @Autowired
+    public AudioController(TrackService trackService, UserChecker userChecker) {
+        this.trackService = trackService;
+        this.userChecker = userChecker;
+    }
+
+    @PutMapping(path = "/track/{trackId}/audio",
+            consumes = "audio/mp3")
+    public ResponseEntity<?> unloadAudio(@RequestParam("audio") MultipartFile audio, @PathVariable Long trackId,
+                                         @RequestHeader("Authorization") String token) throws ApplicationException {
+        if (!userChecker.isAdmin(token)) {
+            throw new ForbiddenException("You have not rights for this");
+        }
+        trackService.saveAudio(audio, trackId);
+
+        return ResponseBuilder.getBuilder().code(200).getResponseEntity();
+    }
+
+    @GetMapping(path = "/track/{trackId}/audio",
+            produces = "audio/mp3")
+    public ResponseEntity<?> getAudio(@PathVariable Long trackId, @RequestHeader("Authorization") String token)
+            throws ApplicationException {
+        userChecker.getUserId(token);
+
+        byte[] audio = trackService.getAudio(trackId);
+        if ((audio == null) || (audio.length == 0)) {
+            throw new ResourceIsNotFoundException("The audio is not exist for this track");
+        }
+        return ResponseEntity.ok(audio);
+    }
+}
