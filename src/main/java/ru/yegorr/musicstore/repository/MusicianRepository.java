8b88db272c264bde6fa9a937d4279bf63b8e1a06
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-24 19:01:49 +0700

    Add findAllByNameContainingIgnoreCase method in MusicianRepository

--- src/main/java/ru/yegorr/musicstore/repository/MusicianRepository.java
+++ src/main/java/ru/yegorr/musicstore/repository/MusicianRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yegorr.musicstore.entity.MusicianEntity;
 
+import java.util.List;
+
 @Repository
 public interface MusicianRepository extends JpaRepository<MusicianEntity, Long> {
-
+    List<MusicianEntity> findAllByNameContainingIgnoreCase(String search);
 }
