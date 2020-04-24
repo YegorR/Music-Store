2657f5c98084b2cb065685c1decc35e5861c6421
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-24 18:59:02 +0700

    Add findAllByNameContainingIgnoreCase method in TrackRepository

--- src/main/java/ru/yegorr/musicstore/repository/TrackRepository.java
+++ src/main/java/ru/yegorr/musicstore/repository/TrackRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yegorr.musicstore.entity.TrackEntity;
 
+import java.util.List;
+
 @Repository
 public interface TrackRepository extends JpaRepository<TrackEntity, Long> {
-
+    List<TrackEntity> findAllByNameContainingIgnoreCase(String search);
 }
