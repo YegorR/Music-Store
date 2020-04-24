56b9386ee5e7db8460adfd12775775000bef6f42
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-24 19:00:41 +0700

    Add findAllByNameContainingIgnoreCase method in AlbumRepository

--- src/main/java/ru/yegorr/musicstore/repository/AlbumRepository.java
+++ src/main/java/ru/yegorr/musicstore/repository/AlbumRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yegorr.musicstore.entity.AlbumEntity;
 
+import java.util.List;
+
 @Repository
 public interface AlbumRepository extends JpaRepository<AlbumEntity, Long> {
-
+    List<AlbumEntity> findAllByNameContainingIgnoreCase(String search);
 }
