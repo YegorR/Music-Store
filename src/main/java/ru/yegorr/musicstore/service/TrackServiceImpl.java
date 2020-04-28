bbf8534dd0a111f48860316f392d74c512fdb91c
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-28 22:31:02 +0700

    Add getTrackInfo method in TrackServiceImpl

--- src/main/java/ru/yegorr/musicstore/service/TrackServiceImpl.java
+++ src/main/java/ru/yegorr/musicstore/service/TrackServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import ru.yegorr.musicstore.dto.response.TrackResponseDto;
 import ru.yegorr.musicstore.entity.TrackEntity;
 import ru.yegorr.musicstore.exception.ApplicationException;
 import ru.yegorr.musicstore.exception.ResourceIsNotFoundException;
@@ -30,7 +31,6 @@ public class TrackServiceImpl implements TrackService {
         } catch (IOException ex) {
             throw new ApplicationException("Error with audio file", ex);
         }
-
     }
 
     @Override
@@ -38,5 +38,20 @@ public class TrackServiceImpl implements TrackService {
     public byte[] getAudio(Long trackId) throws ApplicationException {
         TrackEntity track = trackRepository.findById(trackId).orElseThrow(() -> new ResourceIsNotFoundException("The track is not exist"));
         return track.getAudio();
+    }
+
+    @Override
+    @Transactional
+    public TrackResponseDto getTrackInfo(Long trackId) throws ApplicationException {
+        TrackEntity track = trackRepository.findById(trackId).orElseThrow(() -> new ResourceIsNotFoundException("The track is not exist"));
+        TrackResponseDto response = new TrackResponseDto();
+        response.setId(trackId);
+        response.setName(track.getName());
+        response.setAlbumId(track.getAlbum().getAlbumId());
+        response.setAlbumName(track.getAlbum().getName());
+        response.setMusicianId(track.getAlbum().getMusician().getMusicianId());
+        response.setMusicianName(track.getAlbum().getMusician().getName());
+
+        return response;
     }
 }
