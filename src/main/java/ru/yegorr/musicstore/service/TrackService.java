37f72c2d9cbb5734183d7a725446978f9081995b
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-04-28 22:19:47 +0700

    Add getTrackInfo method in TrackService

--- src/main/java/ru/yegorr/musicstore/service/TrackService.java
+++ src/main/java/ru/yegorr/musicstore/service/TrackService.java
@@ -1,10 +1,13 @@
 package ru.yegorr.musicstore.service;
 
 import org.springframework.web.multipart.MultipartFile;
+import ru.yegorr.musicstore.dto.response.TrackResponseDto;
 import ru.yegorr.musicstore.exception.ApplicationException;
 
 public interface TrackService {
     void saveAudio(MultipartFile audio, Long trackId) throws ApplicationException;
 
     byte[] getAudio(Long trackId) throws ApplicationException;
+
+    TrackResponseDto getTrackInfo(Long trackId) throws ApplicationException;
 }
