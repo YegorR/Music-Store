c49b04bfb951bd42285c266e393641959be67d22
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-03 19:58:12 +0700

    Create BriefAlbumDescriptionDto

---
+++ src/main/java/ru/yegorr/musicstore/dto/response/BriefAlbumDescriptionDto.java
@@ -0,0 +1,58 @@
+package ru.yegorr.musicstore.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public class BriefAlbumDescriptionDto {
+    private Long id;
+
+    private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate releaseDate;
+
+    private boolean single;
+
+    private MusicianBriefResponseDto musician;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isSingle() {
+        return single;
+    }
+
+    public void setSingle(boolean single) {
+        this.single = single;
+    }
+
+    public MusicianBriefResponseDto getMusician() {
+        return musician;
+    }
+
+    public void setMusician(MusicianBriefResponseDto musician) {
+        this.musician = musician;
+    }
+}
