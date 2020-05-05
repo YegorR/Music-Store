8989c7abb7d5b248646e520ec617dc0942864f3b
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-05 18:15:20 +0700

    Add countMusicianCountByAbc method in MusicianRepository

--- src/main/java/ru/yegorr/musicstore/repository/MusicianRepository.java
+++ src/main/java/ru/yegorr/musicstore/repository/MusicianRepository.java
@@ -1,14 +1,20 @@
 package ru.yegorr.musicstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.yegorr.musicstore.entity.MusicianEntity;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface MusicianRepository extends JpaRepository<MusicianEntity, Long> {
     List<MusicianEntity> findAllByNameContainingIgnoreCase(String search);
 
-    int countAllByNameStartingWithIgnoreCase(String letter);
+
+    @Query("SELECT SUBSTRING(musician.name, 1, 1), COUNT(musician) " +
+            "FROM MusicianEntity musician " +
+            "GROUP BY SUBSTRING(musician.name, 1, 1)")
+    Map<String, Integer> getMusicianCountByAbc();
 }
