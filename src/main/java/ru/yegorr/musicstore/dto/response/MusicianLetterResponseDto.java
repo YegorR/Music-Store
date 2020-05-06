0da087c0324cfc3ea53fd57c62cf232b2a7c1e21
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-06 22:48:19 +0700

    Create MusicianLetterResponseDto

---
+++ src/main/java/ru/yegorr/musicstore/dto/response/MusicianLetterResponseDto.java
@@ -0,0 +1,23 @@
+package ru.yegorr.musicstore.dto.response;
+
+public class MusicianLetterResponseDto {
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
