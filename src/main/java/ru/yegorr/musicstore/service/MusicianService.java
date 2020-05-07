1b06f456a62c55c63d455bd1183224a34ad68ad0
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-07 17:32:05 +0700

    Make saveImage method in MusicianService

--- src/main/java/ru/yegorr/musicstore/service/MusicianService.java
+++ src/main/java/ru/yegorr/musicstore/service/MusicianService.java
@@ -1,5 +1,6 @@
 package ru.yegorr.musicstore.service;
 
+import org.springframework.web.multipart.MultipartFile;
 import ru.yegorr.musicstore.dto.response.MusicianLetterResponseDto;
 import ru.yegorr.musicstore.dto.response.MusicianResponseDto;
 import ru.yegorr.musicstore.exception.ApplicationException;
@@ -19,4 +20,6 @@ public interface MusicianService {
     Map<String, Integer> getMusicianCountByAbc() throws ApplicationException;
 
     List<MusicianLetterResponseDto> getMusiciansByLetter(String letter) throws ApplicationException;
+
+    void saveImage(Long musicianId, MultipartFile image) throws ApplicationException;
 }
