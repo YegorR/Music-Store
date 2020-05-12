f460be2a36876919a53c63b76f86c223273ca63b
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-12 23:08:58 +0700

    Create FavouriteRepository

---
+++ src/main/java/ru/yegorr/musicstore/repository/FavouriteRepository.java
@@ -0,0 +1,6 @@
+package ru.yegorr.musicstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavouriteRepository extends JpaRepository<FavouriteRepository, Long> {
+}
