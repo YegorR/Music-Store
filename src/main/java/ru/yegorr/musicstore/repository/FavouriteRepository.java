01a23e765e15e55b54366c484ff9d70ece89b335
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-12 23:23:41 +0700

    Add FavouriteRepository methods

--- src/main/java/ru/yegorr/musicstore/repository/FavouriteRepository.java
+++ src/main/java/ru/yegorr/musicstore/repository/FavouriteRepository.java
@@ -2,7 +2,15 @@ package ru.yegorr.musicstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.yegorr.musicstore.entity.FavouriteEntity;
+import ru.yegorr.musicstore.entity.TrackEntity;
+import ru.yegorr.musicstore.entity.UserEntity;
+
+import java.util.Optional;
 
 @Repository
-public interface FavouriteRepository extends JpaRepository<FavouriteRepository, Long> {
+public interface FavouriteRepository extends JpaRepository<FavouriteEntity, Long> {
+    Optional<FavouriteEntity> findByUserAndTrack(UserEntity user, TrackEntity track);
+
+    void deleteByUserAndTrack(UserEntity user, TrackEntity track);
 }
