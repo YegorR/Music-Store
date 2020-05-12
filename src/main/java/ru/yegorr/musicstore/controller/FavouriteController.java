1f4f44915b287f66a7e06492c6d9d464c82048a7
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-13 00:22:24 +0700

    Create FavouriteController

---
+++ src/main/java/ru/yegorr/musicstore/controller/FavouriteController.java
@@ -0,0 +1,44 @@
+package ru.yegorr.musicstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yegorr.musicstore.dto.request.SetFavouriteRequest;
+import ru.yegorr.musicstore.dto.response.ResponseBuilder;
+import ru.yegorr.musicstore.dto.response.TrackFullResponseDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
+import ru.yegorr.musicstore.service.FavouriteService;
+
+import java.util.List;
+
+@RestController
+public class FavouriteController {
+
+    private final FavouriteService favouriteService;
+
+    private final UserChecker userChecker;
+
+    @Autowired
+    public FavouriteController(FavouriteService favouriteService, UserChecker userChecker) {
+        this.favouriteService = favouriteService;
+        this.userChecker = userChecker;
+    }
+
+    @PutMapping(path = "/favourite")
+    public ResponseEntity<?> setFavourite(@RequestBody SetFavouriteRequest favouriteRequest,
+                                          @RequestHeader String token) throws ApplicationException {
+        Long userId = userChecker.getUserIdOrThrow(token);
+
+        favouriteService.setFavourite(userId, favouriteRequest.getId(), favouriteRequest.isFavourite());
+
+        return ResponseBuilder.getBuilder().code(200).body(favouriteRequest).getResponseEntity();
+    }
+
+    @GetMapping(path = "/favourite")
+    public ResponseEntity<?> getFavourite(@RequestHeader String token) throws ApplicationException {
+        Long userId = userChecker.getUserIdOrThrow(token);
+
+        List<TrackFullResponseDto> response = favouriteService.getFavouriteList(userId);
+        return ResponseBuilder.getBuilder().code(200).body(response).getResponseEntity();
+    }
+}
