db7df8528adabb245ceb8ffe9984587900b40469
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-28 01:33:46 +0700

    Create PlaylistService

---
+++ src/main/java/ru/yegorr/musicstore/service/PlaylistService.java
@@ -0,0 +1,27 @@
+package ru.yegorr.musicstore.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import ru.yegorr.musicstore.dto.request.IdDto;
+import ru.yegorr.musicstore.dto.response.PlaylistBriefDto;
+import ru.yegorr.musicstore.dto.response.PlaylistResponseDto;
+import ru.yegorr.musicstore.exception.ApplicationException;
+
+import java.util.List;
+
+public interface PlaylistService {
+    PlaylistResponseDto getPlaylist(Long playlistId) throws ApplicationException;
+
+    PlaylistResponseDto createPlaylist(Long userId, String name) throws ApplicationException;
+
+    PlaylistResponseDto changePlaylist(Long userId, Long playlistId, String name, List<IdDto> tracks) throws ApplicationException;
+
+    void deletePlaylist(Long userId, Long playlistId) throws ApplicationException;
+
+    void unloadPlaylistImage(Long userId, Long playlistId, MultipartFile image) throws ApplicationException;
+
+    byte[] getPlaylistImage(Long playlistId) throws ApplicationException;
+
+    List<PlaylistBriefDto> searchPlaylists(String query) throws ApplicationException;
+
+    List<PlaylistBriefDto> getPlaylistsOfUser(Long userId) throws ApplicationException;
+}
