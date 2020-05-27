5f4e211d2bda8c79de144f0d99979ce20556de6c
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-05-28 01:01:42 +0700

    Create PlaylistRepository

---
+++ src/main/java/ru/yegorr/musicstore/repository/PlaylistRepository.java
@@ -0,0 +1,12 @@
+package ru.yegorr.musicstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.yegorr.musicstore.entity.PlaylistEntity;
+
+import java.util.List;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<PlaylistEntity, Long> {
+    List<PlaylistEntity> findAllByNameContainingIgnoreCase(String query);
+}
