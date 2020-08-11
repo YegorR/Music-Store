944ded0cc1abe104f6b92fb3c6ca02be6006ad5d
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-08-11 17:54:10 +0700

    Create SuddenException

---
+++ src/main/java/ru/yegorr/musicstore/exception/SuddenException.java
@@ -0,0 +1,11 @@
+package ru.yegorr.musicstore.exception;
+
+public class SuddenException extends RuntimeException {
+    public SuddenException(String message) {
+        super(message);
+    }
+
+    public SuddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
